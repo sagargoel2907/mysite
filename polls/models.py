a70be9c7614527465e65a86da9a8aa9d25773f2c
Sagar Goel <sagargoel2907@gmail.com>
2024-07-25 14:33:24 +0530

    data models task

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.shortcuts import reverse
 # Create your models here.
+
+
+class Question(models.Model):
+
+    text = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = ("Question")
+        verbose_name_plural = ("Questions")
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
+
+class Choice(models.Model):
+
+    text = models.CharField(max_length=25)
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = ("Choice")
+        verbose_name_plural = ("Choices")
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
