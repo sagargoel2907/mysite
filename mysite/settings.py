b03f53129aaed06a30d46b8595198dedeb5e6e04
Sagar Goel <sagargoel2907@gmail.com>
2024-08-15 21:14:32 +0530

    add code to many_load.py for loading polls data

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "polls",
     "home",
     "hello",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
---
+++ polls/scripts/many_load.py
@@ -0,0 +1,19 @@
+from polls.models import Question, Choice
+import csv
+from django.utils import timezone
+
+
+def run():
+    Question.objects.delete()
+    Choice.objects.delete()
+
+    fhandler = open("polls/polls_data.csv")
+    reader = csv.reader(fhandler)
+    next(reader)
+    now = timezone.now()
+    for row in reader:
+        q, c1, c2 = row
+        q = Question(question_text=q, pub_date=now)
+        q.save()
+        Choice(choice_text=c1, question=q).save()
+        Choice(choice_text=c2, question=q).save()
