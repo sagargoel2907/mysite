0b37e312feac4f647662225e58e9eb00ac1c3f00
Sagar Goel <sagargoel2907@gmail.com>
2024-09-04 23:31:58 +0530

    mysql db setup for Classified Ads #1 task

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,13 +86,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sagargoel$ads',
+        'USER': 'sagargoel',
+        'PASSWORD': 'sagar123',
+        'HOST': 'sagargoel.mysql.pythonanywhere-services.com',
+         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
