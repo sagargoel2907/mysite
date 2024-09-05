adb80b8bcd3b93598917b8feec32e107dfd5a834
Sagar Goel <sagargoel2907@gmail.com>
2024-09-06 01:37:55 +0530

    add views, templates for ads app

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
+app_name = 'ads'
 urlpatterns = [
+    path('', views.AdListView.as_view(), name="all"),
+    path('ad/create', views.AdCreateView.as_view(
+        success_url=reverse_lazy('ads:all')), name="ad_create"),
+    path('ad/<int:pk>', views.AdDetailView.as_view(), name="add_detail"),
+    path('ad/<int:pk>/update', views.AdUpdateView.as_view(
+        success_url=reverse_lazy('ads:all')), name="add_update"),
+    path('ad/<int:pk>/delete', views.AdDeleteView.as_view(
+        success_url=reverse_lazy('ads:all')), name="add_delete"),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from ads.models import Ad
 
 # Create your views here.
+
+
+class AdListView(OwnerListView):
+    model = Ad
+    # template_name = 'ads/ad_list.html'
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+    # template_name = 'ads/ad_detail.html'
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    # template_name = 'ads/ad_create.html'
+    fields = ['title', 'price', 'text']
+
+
+class AdUpdateView(OwnerCreateView):
+    model = Ad
+    # template_name = 'ads/ad_update.html'
+    fields = ['title', 'price', 'text']
+    fields_exclude = ['owner', 'created_at', 'updated_at']
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
