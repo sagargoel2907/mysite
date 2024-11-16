b30653fce4d1e660ea9247096cc3f1173e4d5895
Sagar Goel <sagargoel2907@gmail.com>
2024-11-16 21:12:39 +0530

    add comment table to admin page

--- ads/admin.py
+++ ads/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ads.models import Ad
+from ads.models import Ad, Comment
 
 # Register your models here.
 admin.site.register(Ad)
+admin.site.register(Comment)
