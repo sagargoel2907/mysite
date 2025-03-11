9a2e82c5bf0fc1d69edf1968430910f97e634724
Sagar Goel <sagargoel2907@gmail.com>
2025-03-11 22:40:23 +0530

    update solo app test.py to test LoginRequired views

--- solo/tests.py
+++ solo/tests.py
@@ -1,31 +1,61 @@
 from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 from .views import MyView
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your tests here.
 
 
 class MyTestcase(TestCase):
     def test_my_view(self):
-        factory = RequestFactory()
-        request = factory.get(reverse('solo:solo'))
-        view = MyView.as_view()
+        '''
+        Testing the solo view via sending request using RequesTFactory.
+        This will not work for LoginRequired views
+        '''
+        # factory = RequestFactory()
+        # request = factory.get(reverse('solo:solo'))
+        # view = MyView.as_view()
 
-        response = view(request)
-        self.assertEqual(response.status_code, 200)
+        # response = view(request)
+        # self.assertEqual(response.status_code, 200)
 
-        client = Client()
-        response = client.get(reverse('solo:solo'))
-        print(response)  
+        '''
+        Testing the solo view via sending request via Client.
+        This will not work for LoginRequired views
+        # client = Client()
+
+        or we can use self.client as it available from parent TestCase class
+        '''
+        # response = self.client.get(reverse('solo:solo'))
+        # print(response)
+        # self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'solo/form.html')
+
+        # response = self.client.post(reverse('solo:solo'), data={
+        #                        'field1': 'Sagar', 'field2': 'Goel'})
+
+        # print(response)
+        # print(response.context)
+        # print(type(response.context))
+        # self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'solo/form.html')
+        # self.assertEqual(response.context['result'], 'sagar goel')
+
+        '''
+        For view that require Login, we need to simulate a login or do login programmatically
+        using login function
+        '''
+        test_user1 = User.objects.create_user(
+            username='test1', password='test1')
+        # self.client.force_login(test_user1) this will also work
+        self.client.login(username='test1', password='test1')
+        response = self.client.get(reverse('solo:solo'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response,'solo/form.html')
-        
-        response = client.post(reverse('solo:solo'), data={'field1':'Sagar', 'field2':'Goel'})
-        
-        print(response)  
-        print(response.context)
-        print(type(response.context))
+        self.assertTemplateUsed(response, 'solo/form.html')
+
+        response = self.client.post(reverse('solo:solo'), data={
+            'field1': 'Sagar', 'field2': 'Goel'})
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response,'solo/form.html')
-        self.assertEqual(response.context['result'],'sagar goel')
-        
+        self.assertTemplateUsed(response, 'solo/form.html')
+        self.assertEqual(response.context['result'], 'sagar goel')
